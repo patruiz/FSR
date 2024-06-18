3b0815f875dceb9616ac03361ac371c18265c42a
patrickruiz1 <pruiz7@me.com>
2024-06-17 21:04:11 -0700

    uncertainty propagation code, figured out the variables

--- src/uncertainty/uncertainty_calc.py
+++ src/uncertainty/uncertainty_calc.py
@@ -1,6 +1,7 @@
+import os 
 import sympy as sym
 
-def calc_uncertainty(a, b):
+def uncertainty_propagation(V_ref, R1, R2, V2, a, b):
     """
     V_ref = x1
     R1 = x2
@@ -8,16 +9,18 @@ def calc_uncertainty(a, b):
     V2 = x4
     """
     
-    x_1, x_2, x_3, x_4, a, b = sym.symbols('x_1 x_2 x_3 x_4 a b')
+    x_1, x_2, x_3, x_4, A, B = sym.symbols('x_1 x_2 x_3 x_4 A B')
 
-    F = (-(.0016*x_1*x_2*x_3)/(a*(x_4-.004*x_3)))**(1/b)
+    F = (-(.0016*x_1*x_2*x_3)/(A*(x_4-.004*x_3)))**(1/B)
 
-    diff_x1 = F.diff(x_1)
-    diff_x2 = F.diff(x_2)
-    diff_x3 = F.diff(x_3)
-    diff_x4 = F.diff(x_4)
+    partial_derivatives = [F.diff(var) for var in (x_1, x_2, x_3, x_4)]
 
-    print(diff_x1)
-    print(diff_x2)
-    print(diff_x3)
-    print(diff_x4)
+    subs = {x_1: V_ref, x_2: R1, x_3: R2, x_4: V2, A: a, B:b}
+
+    evaluated_derivatives = [diff.subs(subs) for diff in partial_derivatives]
+    
+    return evaluated_derivatives
+
+    
+
+print(uncertainty_propagation(24, 1500, 240, -24, 1002, -9123))
